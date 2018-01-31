32a4475931abdbbf2a9566b56a2e22aa3520548c
Petr <32465014+kutekhov@users.noreply.github.com>
2018-02-01 00:16:35 +0300

    complete in January
    solved

---
+++ array-3/src/countClumps.java
@@ -0,0 +1,21 @@
+public class countClumps {
+    public int countClumps(int[] nums) {
+        int count = 0;
+        int i = 0;
+
+        while(i < nums.length) {
+            int val = nums[i];
+            i++;
+            int length = 1;
+            while(i < nums.length && nums[i] == val) {
+                i++;
+                length++;
+            }
+
+            if(length > 1)
+                count++;
+        }
+
+        return count;
+    }
+}
