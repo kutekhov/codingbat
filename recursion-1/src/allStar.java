da441f8bc52d8dc33d63b31257a9d26217a6875d
Petr <32465014+kutekhov@users.noreply.github.com>
2018-02-02 18:26:49 +0300

    complete in december
    solved tasks recursion-1

---
+++ recursion-1/src/allStar.java
@@ -0,0 +1,9 @@
+public class allStar {
+    public String allStar(String str) {
+        if(str.length() <= 1)
+            return str;
+
+        return str.charAt(0) + "*" + allStar(str.substring(1));
+    }
+
+}
---
+++ recursion-1/src/array6.java
@@ -0,0 +1,8 @@
+public class array6 {
+    public boolean array6(int[] nums, int index) {
+        if(index >= nums.length)
+            return false;
+
+        return nums[index] == 6 || array6(nums, index + 1);
+    }
+}
